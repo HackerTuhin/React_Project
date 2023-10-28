b243de286d2dad9ee6d6bfe1c6a553493d05f374
CoderTuhin <86509674+HackerTuhin@users.noreply.github.com>
2023-10-28 13:37:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+
+function Myfun()
+{
+    let val=-77;
+    if(val>0)
+    {
+        return (
+        <div>
+            <h1>the value is greater then 0</h1>
+            <p>{val}</p>
+        </div>
+        );
+    }else if(val<0)
+    {
+        return (
+        <div>
+           <h1>the value is less then 0</h1>
+            <p>{val}</p>
+        </div>
+        );
+    }
+    else{
+        return (<div><h1>the value is eual to zero </h1></div>);
+    }
+}
+   
+
+ReactDOM.render(
+<div>
+    <Myfun />
+</div>
+, document.getElementById("test"));
